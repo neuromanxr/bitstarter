50e4426d7f5aee0111459dcdbdb92eb9434a42b8
neuromanxr <kelv17@gmail.com>
2014-07-31 04:38:10 +0000

    Reading from file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var string = fs.readFileSync("index.html", "utf8");
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World! 2');
+  response.send(string);
 });
 
 app.listen(app.get('port'), function() {
